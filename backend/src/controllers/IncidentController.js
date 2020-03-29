d088d8f86720ed3c1b3c200c048bf6b78302031c
JuanJGB <juanbenini123@gmail.com>
2020-03-29 16:17:12 -0300

    Mobile da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -24,7 +24,7 @@ module.exports = {
     
     async create(request, response) {
       const { title, description, value } = request.body;
-      const ong_id = request.headers.athorization;
+      const ong_id = request.headers.authorization;
          
       const [id] = await connection('incidents').insert({
           title,
